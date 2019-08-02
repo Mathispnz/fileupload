8ec5b5273f41aeb74e32cf3c6acac4c2ada44532
Mathis PEINGNEZ <mathispeingnez@iMac-de-Carine-Patureau.local>
2019-08-02 15:31:13 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(fileUpload);
+
+// Upload Endpoint
+app.post('/upload', (req, res, next) => {
+    if (req.files === nul) {
+        return res.status(400).json({ msg: 'No file uploaded' })
+    }
+    const file = req.files.file;
+    file.mv(`${__dirname}/client/public/upload/${file.name}`, err => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err)
+        }
+
+        res.json({ fileName: file.name, filePath: `/upload/${file.name}`});
+    })
+})
+
+app.listen(5000, () => {
+    console.log('Server started');
+})
